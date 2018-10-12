34637972449e34d31b5f3bfe9f41c2ac38c74d6a
kevin <kevinz@weghst.com>
2018-10-12 19:34:43 +0800

    增加代理

--- vue.config.js
+++ vue.config.js
@@ -1,44 +1,22 @@
 const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
 
 module.exports = {
-    chainWebpack: config => {
-    config.resolve.alias
-    .set('@', resolve('src/main/web'))
-
-config.plugin('copy')
-    .use(CopyWebpackPlugin, [[{
-        from: './node_modules/monaco-editor/min',
-        to: 'monaco-editor/min'
-    }, {
-        from: './node_modules/monaco-editor/min-maps',
-        to: 'monaco-editor/0.11.1/min-maps'
-    }, {
-        from: './src/main/web/statics/fontawesome-free-5.0.10',
-        to: 'fontawesome-free-5.0.10/'
-    }]])
-},
-pages: {
-    index: {
-        entry: 'src/main/web/index.js',
-            template: 'src/main/web/templates/index.html',
+    pages: {
+        index: {
+            entry: 'src/main/web/main.js',
+            template: 'src/main/web/index.html',
             filename: 'index.html'
+        }
     },
-    oai: {
-        entry: 'src/main/web/oai.js',
-            template: 'src/main/web/oai.html',
-            filename: 'oai.html'
-    }
-},
-devServer: {
-    proxy: {
-        '/api': {
-            target: 'http://localhost:7777'
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:7779'
+            }
         }
     }
-}
 }
