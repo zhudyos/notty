419d1ef0e7840b4a8d8350eb4709d2959656611f
kevin <kevinz@weghst.com>
2018-11-13 15:33:50 +0800

    新增 copy-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,19 @@
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
 
 module.exports = {
+    configureWebpack: {
+        plugins: [
+            new CopyWebpackPlugin([{
+                from: 'src/main/web/public/img/logo.png',
+                to: 'logo.png'
+            }])
+        ]
+    },
     pages: {
         index: {
             entry: 'src/main/web/main.js',
